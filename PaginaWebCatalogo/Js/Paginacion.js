f960677f3df8ec2a607e81577a0fb7e64f562374
Alexander Mata <amata@insservicios.com>
2019-12-23 09:54:06 -0600

    Se corrige el problema de la paginacion y se aplican algunos estilos

--- PaginaWebCatalogo/Js/Paginacion.js
+++ PaginaWebCatalogo/Js/Paginacion.js
@@ -9,24 +9,24 @@
             this.totalPages = Math.ceil(this.items.length / this.perPage);
 
             $('.pagination', this.container.parent()).remove();
-            var pagination = $('<div class="pagination"></div>').append('<a class="nav prev disabled" data-next="false"><</a>');
+            var pagination = $('<div style="display: table;margin: 0 auto;margin-top:-100px;" class="pagination"></div>').append('<li class="page-item"><a class="Paginacion prev disabled" data-next="false">Anterior</a>');
 
             for (var i = 0; i < this.totalPages; i++) {
                 var pageElClass = "page";
                 if (!i)
                     pageElClass = "page current";
-                var pageEl = '<a class="' + pageElClass + '" data-page="' + (
+                var pageEl = '<li class="page-item"><a class="' + pageElClass + '" data-page="' + (
                     i + 1) + '">' + (
-                        i + 1) + "</a>";
+                        i + 1) + "</a></li>";
                 pagination.append(pageEl);
             }
-            pagination.append('<a class="nav next" data-next="true">></a>');
+            pagination.append('<li class="page-item"><a  class="Paginacion next page-item" data-next="true">Siguiente</a></li>');
 
             this.container.after(pagination);
 
             var that = this;
-            $("body").off("click", ".nav");
-            this.navigator = $("body").on("click", ".nav", function () {
+            $("body").off("click", ".Paginacion");
+            this.navigator = $("body").on("click", ".Paginacion", function () {
                 var el = $(this);
                 that.navigate(el.data("next"));
             });
@@ -42,20 +42,20 @@
             if (isNaN(next) || next === undefined) {
                 next = true;
             }
-            $(".pagination .nav").removeClass("disabled");
+            $(".pagination .Paginacion").removeClass("disabled");
             if (next) {
                 this.currentPage++;
                 if (this.currentPage > (this.totalPages - 1))
                     this.currentPage = (this.totalPages - 1);
                 if (this.currentPage == (this.totalPages - 1))
-                    $(".pagination .nav.next").addClass("disabled");
+                    $(".pagination .Paginacion.next").addClass("disabled");
             }
             else {
                 this.currentPage--;
                 if (this.currentPage < 0)
                     this.currentPage = 0;
-                if (this.currentPage == 0)
-                    $(".pagination .nav.prev").addClass("disabled");
+                if (this.currentPage === 0)
+                    $(".pagination .Paginacion.prev").addClass("disabled");
             }
 
             this.showItems();
@@ -71,12 +71,12 @@
 
             this.currentPage = page - 1;
 
-            $(".pagination .nav").removeClass("disabled");
+            $(".pagination .Paginacion").removeClass("disabled");
             if (this.currentPage == (this.totalPages - 1))
-                $(".pagination .nav.next").addClass("disabled");
+                $(".pagination .Paginacion.next").addClass("disabled");
 
             if (this.currentPage == 0)
-                $(".pagination .nav.prev").addClass("disabled");
+                $(".pagination .Paginacion.prev").addClass("disabled");
             this.showItems();
         },
         showItems: function () {
@@ -116,4 +116,4 @@
     };
 })(jQuery);
 
-$(".tabs").pagify(5, ".product-men");
+$(".tab1").pagify(20, ".product-men");
